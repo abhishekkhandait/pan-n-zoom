5ab4109bd0d4bc827f22ac95dd1c7261764b395b
Abhishek Khandait <khandaitabhishek@gmail.com>
2019-05-05 02:42:42 +0530

    getOffsetxy method added

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -1,3 +1,22 @@
+import { Point } from "../types";
+
 export default class Transform {
   constructor(public x = 0, public y = 0, public scale = 1) {}
+
+  public static getOffsetXY(
+    e: MouseEvent | Touch,
+    el: HTMLElement | SVGElement
+  ): Point {
+    var offsetX, offsetY;
+    // I tried using e.offsetX, but that gives wrong results for svg, when user clicks on a path.
+    var ownerRect = el.getBoundingClientRect();
+    offsetX = e.clientX - ownerRect.left;
+    offsetY = e.clientY - ownerRect.top;
+
+    return { x: offsetX, y: offsetY };
+  }
+
+  public static getScreenCTM(svgElement: SVGSVGElement): DOMMatrix {
+    return svgElement.getScreenCTM();
+  }
 }
