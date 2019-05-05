e7fef9f93de6aa401e2e48fc24de45b5fbca0384
Abhishek Khandait <khandaitabhishek@gmail.com>
2019-05-05 13:56:16 +0530

    tslint error

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -1,17 +1,14 @@
 import { Point } from "../types";
 
 export default class Transform {
-  constructor(public x = 0, public y = 0, public scale = 1) {}
-
   public static getOffsetXY(
     e: MouseEvent | Touch,
     el: HTMLElement | SVGElement
   ): Point {
-    var offsetX, offsetY;
     // I tried using e.offsetX, but that gives wrong results for svg, when user clicks on a path.
-    var ownerRect = el.getBoundingClientRect();
-    offsetX = e.clientX - ownerRect.left;
-    offsetY = e.clientY - ownerRect.top;
+    const ownerRect = el.getBoundingClientRect();
+    const offsetX = e.clientX - ownerRect.left;
+    const offsetY = e.clientY - ownerRect.top;
 
     return { x: offsetX, y: offsetY };
   }
@@ -19,4 +16,6 @@ export default class Transform {
   public static getScreenCTM(svgElement: SVGSVGElement): DOMMatrix {
     return svgElement.getScreenCTM();
   }
+
+  constructor(public x = 0, public y = 0, public scale = 1) {}
 }
