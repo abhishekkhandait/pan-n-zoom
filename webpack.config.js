3655a2d99c56d9447ce47bd4df46145624fd699a
Abhishek Khandait <khandaitabhishek@gmail.com>
2019-05-07 01:33:14 +0530

    sass-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
 const path = require("path");
 
 module.exports = (env, argv) => ({
-  entry: "./src/index.ts",
-  devtool: argv.mode === "development" ? "inline-source-map" : "none",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  output: {
-    filename: `pannzoom${argv.mode === "development" ? "" : ".min"}.js`,
-    path: path.resolve(__dirname, "dist"),
-    library: "PanZoom",
-    libraryTarget: "umd"
-  }
+	entry: "./src/index.ts",
+	devtool: argv.mode === "development" ? "inline-source-map" : "none",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					"style-loader", // creates style nodes from JS strings
+					"css-loader", // translates CSS into CommonJS
+					"sass-loader" // compiles Sass to CSS, using Node Sass by default
+				]
+			}
+		]
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"]
+	},
+	output: {
+		filename: `pannzoom${argv.mode === "development" ? "" : ".min"}.js`,
+		path: path.resolve(__dirname, "dist"),
+		library: "PanZoom",
+		libraryTarget: "umd"
+	}
 });
